7d1b89334b8896cdc64a423a5b97449a1b5984d3
stephk98 <stefkos98@gmail.com>
2023-04-21 20:37:44 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~>4.0"
+      version = ">= 2.26"
     }
   }
 }
